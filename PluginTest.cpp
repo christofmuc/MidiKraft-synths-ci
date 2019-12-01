97e52dfdef8ce70e08be99df37a3686356e4a4ae
cruc <christof.ruch@gmx.de>
2019-12-01 22:18:58 +0100

    Reducing the amount of code needed to a minimum, thanks to a template plugin class

--- PluginTest.cpp
+++ PluginTest.cpp
@@ -1,30 +1,14 @@
-#include "RD8.h"
+#include "RD8Plugin.h"
 
 #include "JuceHeader.h"
 
-typedef  midikraft::BehringerRD8 *(*createFunctionType)();
-typedef  void (*deleteFunctionType)(midikraft::BehringerRD8 *);
-
 int main() {
 
-	DynamicLibrary rd8_plugin("RD8");
+	RD8Plugin rd8_plugin;
 
-	if (rd8_plugin.open("midikraft-behringer-rd8.dll")) {
-		auto createFn = (createFunctionType)rd8_plugin.getFunction("CreateObjectInstance");
-
-		if (createFn) {
-			auto *created = (*createFn)();
-			std::cout << created->getName();
-
-			auto deleteFn = (deleteFunctionType)rd8_plugin.getFunction("ReleaseObject");
-
-			if (deleteFn) {
-				(*deleteFn)(created);
-			}
-		}
-
-		rd8_plugin.close();
-	}
+	auto created = rd8_plugin.create();
+	std::cout << created->getName();
+	rd8_plugin.destroy(created);
 
 	return 0;
 }
